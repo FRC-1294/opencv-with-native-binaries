0f53bac3b6d4a2cd46109bbe3e0dc276714a2caf
jxlewis <jxlewis@godaddy.com>
2017-10-28 07:25:32 -0700

    added test for contrib stuff

--- src/test/java/org/usfirst/frc/team1294/opencv/OpenCVTest.java
+++ src/test/java/org/usfirst/frc/team1294/opencv/OpenCVTest.java
@@ -1,14 +1,28 @@
 package org.usfirst.frc.team1294.opencv;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.Core;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.FeatureDetector;
+
 import static org.junit.Assert.*;
 
 public class OpenCVTest {
 
-  @Test
-  public void calLoadNativeLibrariesTest() throws Exception {
+  @Before
+  public void before() {
     OpenCV.loadNativeLibraries();
+  }
+
+  @Test
+  public void canLoadNativeLibrariesTest() {
     assertEquals("3.3.1", Core.VERSION);
+  }
+
+  @Test
+  public void canUseContribFeatures() throws Exception {
+    FeatureDetector detector = FeatureDetector.create(FeatureDetector.SURF);
+    DescriptorExtractor extractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
   }
 }
